a3ca7fa5175f5e29f56511832478395f8816d925
luzhanheng <luzhanhengde@gmail.com>
2025-05-28 21:54:40 +0800

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import HomePage from './HomePage.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomePage from "./HomePage";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HomePage />
   </React.StrictMode>
-)
+);
